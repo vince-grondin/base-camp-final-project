26c47571df4ac797c3c60fffed60949593fdd5dd
Roch <vince-grondin@users.noreply.github.com>
2023-12-20 12:07:54 -1000

    Consume `Property` items from local blockchain, render data accordingly on Home page and Details page (#13)
    Closes #12
    
    Now that the Leasy contract exposes the `getProperties` method, it can be consumed by the Frontend. This set of changes consists of calling the `getProperties` from the Leasy smart contract deployed at `NEXT_PUBLIC_LEASY_CONTRACT_ADDRESS` using the ABI in Foundry's output folder `contracts/out/` and rendering the `Property` items data accordingly on home page and the details page.
    
    - create the `PropertiesContext`, implement the `PropertiesContextProvider` that uses `useContractRead` to call `getProperties` and returns a provider that exposes the fetches `Property` items to children components
    - wrap the root component in `layout.tsx` with the `PropertiesContextProvider` so that `Property` items are available to any components that may needed it
    - call `usePropertiesContext()` on the details page to look up the property identified by the `propertyID` instead of using the fixtures array
    - call `usePropertiesContext()` on the home page to list the properties
    - added `localhost` chain to the wagmi chains config.

--- frontend/src/app/_components/PropertiesGrid.tsx
+++ frontend/src/app/_components/PropertiesGrid.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import PropertyCard from "./PropertyCard"
-import { properties } from "../_fixtures/Properties";
+import { usePropertiesContext } from "../_contexts/state";
 
 export default function PropertiesGrid() {
+    const properties = usePropertiesContext();
+
     return <div className="grid grid-cols-4 gap-4">
         {
             properties.map((property) =>
-                <Link href={`/property/${property.id}`}><PropertyCard key={property.id} {...{ property }} /></Link>
+                <Link key={property.id} href={`/property/${property.id}`}><PropertyCard {...{ property }} /></Link>
             )
         }
     </div>
