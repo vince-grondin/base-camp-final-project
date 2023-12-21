f387248b43fcbd8df3b7807510744e2dc5f03a08
Roch <vince-grondin@users.noreply.github.com>
2023-12-21 11:19:58 -1000

    Render property status in property card and on property details page, render property signers and signature statuses on details page (#17)
    Closes #6
    
    ## Description
    A property owner may initiate the leasing process by calling the new `leaseProperty` method of the `Leasy` contract.
    `Property`s now have a status, a renters array and a signature statuses array.
    This set of changes consists of rendering a property status in the property card and on property details page, as well as renderering the property signers and signature statuses on details page.
    
    ## Changes in this Pull Request
    - modify the `Property` model to reflect the field name changes in the contract
    - add the `status` and `depositAmount` to the `Property` model
    - add the `PropertyStatusBadge` component that renders a colored badge based a property status
    - render the property status badge in the property card
    - render the property status badge on the property details page
    - render the list of renters and their corresponding signature statuses on the property details page

--- frontend/src/app/_components/PropertyCard.tsx
+++ frontend/src/app/_components/PropertyCard.tsx
@@ -1,17 +1,18 @@
 import { Property } from "../Models"
+import { PropertyStatusBadge } from "./PropertyStatusBadge"
 
 export type PropertyCardProps = {
     property: Property
 }
 
-export default function PropertyCard({ property: { id, name, address } }: PropertyCardProps) {
-    return <div className="p-6 bg-white rounded-xl shadow-lg flex items-center space-x-4">
-        <div className="">
-            🏠
+export default function PropertyCard({ property: { id, name, fullAddress, status } }: PropertyCardProps) {
+    return <div className="p-6 bg-white rounded-xl shadow-lg">
+        <div className="mb-2">
+            <span className="text-xl font-medium text-black me-2">🏠 {name}</span>
+            <p className="text-slate-500">{fullAddress}</p>
         </div>
         <div>
-            <div className="text-xl font-medium text-black">{name}</div>
-            <p className="text-slate-500">{address}</p>
+            <PropertyStatusBadge status={status} />
         </div>
     </div>
 }
--- frontend/src/app/property/[propertyID]/page.tsx
+++ frontend/src/app/property/[propertyID]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { SignerStatus } from "@/app/Models";
+import { PropertyStatusBadge } from "@/app/_components/PropertyStatusBadge";
 import { usePropertiesContext } from "@/app/_contexts/state";
 import { useAccount } from "wagmi";
 
@@ -18,41 +19,43 @@ export default function PropertyDetails({ params: { propertyID } }: PropertyDeta
 
     const { address: connectedAddress, isConnecting, isDisconnected } = useAccount();
 
-    const { name, owner, address, leaseAgreementUrl, signers, signersStatuses } = property;
+    const { name, owner, fullAddress, leaseAgreementUrl, renters, signatureStatuses, status } = property;
 
-    function isPdf(documentUrl: string) {
+    function canBeRendered(documentUrl: string) {
         return leaseAgreementUrl.endsWith('.pdf');
     }
 
     return <div>
-        <h1 className="text-3xl block mb-5">🏠 {name}</h1>
-
+        <div className="mb-5">
+            <span className="text-3xl mb-10 me-5">🏠 {name}</span>
+            <PropertyStatusBadge status={status} />
+        </div>
         <h4>👤 Owned by {owner == connectedAddress ? 'you' : owner}</h4>
 
         <span>📍 Located at&nbsp;
-            <a className="underline" target="_blank" href={`https://www.google.com/maps/place/${address}`}>{address}</a>
+            <a className="underline" target="_blank" href={`https://www.google.com/maps/place/${fullAddress}`}>{fullAddress}</a>
         </span>
 
         <hr className="mt-5 mb-5" />
 
         <h2 className="text-2xl mb-2">📃 Lease</h2>
         <h3 className="text-lg">✍️ Signatures</h3>
-        {(!signers || signers.length === 0) &&
+        {(!renters || renters.length === 0) &&
             <div>
-                Lease not sent to any signers.
+                Lease not sent to any renters.
             </div>
         }
-        {signers && signers.length > 0 &&
+        {renters && renters.length > 0 &&
             <ul>
-                {signers.map((address, index) =>
-                    <SignerRow {...{ address, status: signersStatuses[index] || null }} />)
+                {renters.map((address, index) =>
+                    <SignerRow {...{ address, status: signatureStatuses[index] ?? null }} />)
                 }
             </ul>
         }
 
         <h3 className="text-lg mt-5">Agreement</h3>
-        {isPdf(leaseAgreementUrl) && <iframe title="lease" src={leaseAgreementUrl} width={800} height={800} />}
-        {!isPdf(leaseAgreementUrl) && <a className="underline" target="_blank" href={leaseAgreementUrl}>Open 🔗</a>}
+        {canBeRendered(leaseAgreementUrl) && <iframe title="lease" src={leaseAgreementUrl} width={800} height={800} />}
+        {!canBeRendered(leaseAgreementUrl) && <a className="underline" target="_blank" href={leaseAgreementUrl}>Open 🔗</a>}
     </div>
 }
 
@@ -64,9 +67,9 @@ type SignerRowProps = {
 function SignerRow({ address, status }: SignerRowProps) {
     function statusIcon() {
         switch (status) {
-            case SignerStatus.APPROVED: return "✅";
-            case SignerStatus.DECLINED: return "❌";
-            default: return "↗️"
+            case SignerStatus.APPROVED: return <span>✅</span>;
+            case SignerStatus.DECLINED: return <span>❌</span>;
+            case SignerStatus.PENDING: return <span>↗️</span>
         }
     }
 
